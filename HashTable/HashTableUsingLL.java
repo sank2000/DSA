3b2b83dce7b8478d5e9ed376e6a482cfb102df94
sank2000 <santhoshvelr@yahoo.com>
2021-07-29 21:18:08 +0530

    class hashTable using ll ✔

---
+++ HashTable/HashTableUsingLL.java
@@ -0,0 +1,99 @@
+package HashTable;
+
+import java.util.LinkedList;
+
+/**
+ * HashTableUsingLL
+ */
+public class HashTableUsingLL {
+  private class Entry {
+    private int key;
+    private String value;
+
+    public Entry(int k,String v) {
+      key = k;
+      value = v;
+    }
+  }
+
+  LinkedList<Entry>[] hashtable;
+  int SIZE = 5;
+
+  HashTableUsingLL() {
+    hashtable = new LinkedList[SIZE];
+  }
+  
+  HashTableUsingLL(int size) {
+    SIZE = size;
+    hashtable = new LinkedList[SIZE];
+  }
+
+  public void put(int k, String v) {
+    var entry = new Entry(k, v);
+    var hash = getHash(k);
+
+    if (hashtable[hash] == null) {
+      hashtable[hash] = new LinkedList<Entry>();
+    } else {
+      for (Entry e : hashtable[hash]) {
+        if (e.key == k) {
+          e.value = v;
+          return;
+        }
+      }
+    }
+
+    hashtable[hash].addLast(entry);
+  }
+  
+  public String get(int k) {
+    var hash = getHash(k);
+
+    if (hashtable[hash] == null) {
+      return null;
+    }
+
+    for (Entry entry : hashtable[hash]) {
+      if (entry.key == k)
+        return entry.value;
+    }
+
+    return null;
+  }
+  
+  public void remove(int k) {
+    var hash = getHash(k);
+
+    if (hashtable[hash] == null) {
+      return;
+    }
+
+    Entry e = null;
+
+    for (Entry entry : hashtable[hash]) {
+      if (entry.key == k)
+        e = entry;
+    }
+    if (e == null)
+      return;
+
+    hashtable[hash].remove(e);
+  }
+
+  private int getHash(int value) {
+    return value % SIZE;
+  }
+  
+  public void print() {
+    for (LinkedList<Entry> linkedList : hashtable) {
+      if (linkedList == null)
+        continue;
+      System.out.println(linkedList);
+      for (Entry entry : linkedList) {
+        System.out.println(entry.key + " - " + entry.value);
+      }
+    }
+  }
+
+  
+}
--- HashTable/Main.java
+++ HashTable/Main.java
@@ -2,8 +2,17 @@ package HashTable;
 
 public class Main {
   public static void main(String[] args) {
-    NonRepeating nr = new NonRepeating();
-    System.out.println(nr.findFirstCharacter("a green apple"));
-    System.out.println(Repeating.findFirst("a green apple"));
+    // NonRepeating nr = new NonRepeating();
+    // System.out.println(nr.findFirstCharacter("a green apple"));
+    // System.out.println(Repeating.findFirst("a green apple"));
+
+    HashTableUsingLL htl = new HashTableUsingLL();
+    htl.put(1, "san");
+    htl.put(2, "san");
+    htl.put(2, "test");
+    htl.print();
+    System.out.println(htl.get(10));
+    htl.remove(1);
+    htl.print();
   }
 }
